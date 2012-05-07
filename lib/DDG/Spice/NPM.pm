6b0093fcac25c345d4104a028f74129010caf1fb
Patrick Clark <pat@hellop.at>
2012-05-07 14:04:07 -0400

    Adding nginx override
    This should help with the lack of JSONP support from npmjs.org API.
    
    Signed-off-by: Patrick Clark <pat@hellop.at>

--- lib/DDG/Spice/NPM.pm
+++ lib/DDG/Spice/NPM.pm
@@ -4,7 +4,20 @@ use DDG::Spice;
 
 triggers start => "npm";
 
-spice to => 'http://search.npmjs.org/_list/search/search?startkey="$1"&limit=1';
+#spice to => 'http://search.npmjs.org/_list/search/search?startkey="$1"&limit=1';
+
+sub nginx_conf {
+    return <<"__END_OF_CONF__";
+
+location ^~ /js/spice/npm/ {
+    echo_before_body 'ddg_spice_npm(';
+    rewrite ^/js/spice/npm/(.*) /_list/search/search?startkey="\$1"&limit=1 break;
+    proxy_pass http://search.npmjs.org/;
+    echo_after_body ');';
+}
+
+__END_OF_CONF__
+}
 
 handle remainder => sub {
     my $term = $_ || '';
